89b41b9336d66fce9e31ecd32a79311881b9d625
Misael Beray <misaelberay@gmail.com>
2022-04-06 14:53:52 -0300

    refactor(modules): removed unnecessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { databaseProviders } from './database/database.providers';
 import { ChampionsModule } from './champions/champions.module';
-import { ChampionsService } from './champions/champions.service';
-import { ChampionsResolver } from './champions/champions.resolver';
-import { championsProviders } from './champions/champions.providers';
 import { SpecialAttacksModule } from './special-attacks/special-attacks.module';
-import { SpecialAttacksService } from './special-attacks/special-attacks.service';
-import { SpecialAttacksResolver } from './special-attacks/special-attacks.resolver';
-import { specialAttacksProviders } from './special-attacks/special-attacks.providers';
 import { TagsModule } from './tags/tags.module';
 import { SkillsModule } from './skills/skills.module';
 import { SinergiesModule } from './sinergies/sinergies.module';
@@ -43,13 +37,7 @@ import { GqlJwtAuthGuard } from './common/guards/gql-jwt-auth.guard'
       useClass: GqlJwtAuthGuard
     },
     AppService,
-    ChampionsService,
-    ChampionsResolver,
-    SpecialAttacksService,
-    SpecialAttacksResolver,
     ...databaseProviders,
-    ...championsProviders,
-    ...specialAttacksProviders,
   ],
 })
 export class AppModule {}
